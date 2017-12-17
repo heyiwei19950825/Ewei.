0aa7077290a4a60a7c2f838f16639c4be0d1eee0
Ewei <heyiwei492007413@qq.com>
2017-12-17 22:05:24 +0800

    update save

--- application/api/controller/v1/Upload.php
+++ application/api/controller/v1/Upload.php
@@ -3,6 +3,8 @@ namespace app\api\controller\v1;
 
 use think\Controller;
 use think\Session;
+use think\Image;
+use think\File;
 
 /**
  * 通用上传接口
@@ -35,16 +37,27 @@ class Upload extends Controller
             'ext'  => 'jpg,gif,png,bmp'
         ];
 
-        $file = $this->request->file('file');
-
-        $upload_path = str_replace('\\', '/', ROOT_PATH . 'public/uploads');
+        $file        = $this->request->file('file');
+        $data        = $this->request->param();
+ 
+        $upload_path = str_replace('\\', '/', ROOT_PATH . 'public/uploads/');
         $save_path   = '/uploads/';
         $info        = $file->validate($config)->move($upload_path);
+        $time        = date('Ymd',time());
 
         if ($info) {
-            $result = [
-                'error' => 0,
-                'url'   => str_replace('\\', '/', $save_path . $info->getSaveName())
+            //创建缩略图
+            $image    = Image::open( UPLOAD_PATH . $info->getSaveName() );
+            if ( !is_dir('./uploads/thumb/'.$time )) {//检测文件夹是否存在
+                mkdir('./uploads/thumb/'.$time);//创建缩略图日期文件
+            }
+            // 按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.png
+            $imageRow = $image->thumb($data['width'], $data['height'])->save('./uploads/thumb/'. $info->getSaveName());
+
+            $result   = [
+                'error'   => 0,
+                'url'     => str_replace('\\', '/', '/uploads/thumb/' . $info->getSaveName()),
+                'img_row' => $imageRow->size()
             ];
         } else {
             $result = [
--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 define('LOG_PATH', __DIR__. '/../log/');
+define('UPLOAD_PATH', __DIR__. '/uploads/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
 
